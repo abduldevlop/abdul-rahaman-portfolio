b67ee7750d877466cea4b67654fdf26f6c153cd8
Abdul <abduldevelop@gmail.com>
2024-03-28 14:12:02 +0530

    add project filtering options

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -10,6 +10,41 @@ const Projects = () => {
   const [showModal, setShowModal] = useState(false);
   const [selectedProject, setSelectedProject] = useState(null);
 
+  const [filters, setFilters] = useState({
+    react: false,
+    next: false,
+    tailwind: false,
+  });
+
+  const handleFilterChange = (event: any) => {
+    const { name, checked } = event.target;
+    setFilters({
+      ...filters,
+      [name]: checked,
+    });
+  };
+  const filteredProjects = projects.filter((project: Tproject) => {
+    if (
+      filters.react &&
+      !project.techStack.some((tech) => tech.includes("Reactjs"))
+    ) {
+      return false;
+    }
+    if (
+      filters.next &&
+      !project.techStack.some((tech) => tech.includes("Nextjs"))
+    ) {
+      return false;
+    }
+    if (
+      filters.tailwind &&
+      !project.techStack.some((tech) => tech.includes("TailwindCSS"))
+    ) {
+      return false;
+    }
+    return true;
+  });
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -43,39 +78,75 @@ const Projects = () => {
       </div>
       <div className="w-full flex">
         <p className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]">
-          Following projects showcases my skills and experience through
+          Following projects showcase my skills and experience through
           real-world examples of my work. Each project is briefly described with
           links to code repositories and live demos in it. It reflects my
           ability to solve complex problems, work with different technologies,
           and manage projects effectively.
         </p>
       </div>
-      <div className="mt-20 flex flex-wrap gap-7">
-        {projects
-          .slice()
-          .reverse()
-          .map((project: Tproject) =>
-            project.enabled ? (
-              <div
-                key={project._id}
-                className="p-5 rounded-2xl sm:w-[360px] w-full shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px] cursor-pointer"
-                onClick={() => handleProjectClick(project)}
-              >
-                <div className="relative w-full h-[230px] ">
-                  <img
-                    src={project.image.url}
-                    alt="project_image"
-                    className="w-full h-full object-cover rounded-2xl "
-                  />
+      <div className="mt-10 flex space-x-4">
+        <label className="inline-flex items-center">
+          <input
+            type="checkbox"
+            name="react"
+            checked={filters.react}
+            onChange={handleFilterChange}
+            className="form-checkbox text-primary border-primary"
+          />
+          <span className="ml-2">React.js</span>
+        </label>
+        <label className="inline-flex items-center">
+          <input
+            type="checkbox"
+            name="next"
+            checked={filters.next}
+            onChange={handleFilterChange}
+            className="form-checkbox text-primary border-primary"
+          />
+          <span className="ml-2">Next.js</span>
+        </label>
+        <label className="inline-flex items-center">
+          <input
+            type="checkbox"
+            name="tailwind"
+            checked={filters.tailwind}
+            onChange={handleFilterChange}
+            className="form-checkbox text-primary border-primary"
+          />
+          <span className="ml-2">Tailwind CSS</span>
+        </label>
+      </div>
+      <div className="mt-5 flex flex-wrap gap-7">
+        {filteredProjects.length > 0 ? (
+          filteredProjects
+            .slice()
+            .reverse()
+            .map((project: Tproject) =>
+              project.enabled ? (
+                <div
+                  key={project._id}
+                  className="p-5 rounded-2xl sm:w-[360px] w-full shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px] cursor-pointer"
+                  onClick={() => handleProjectClick(project)}
+                >
+                  <div className="relative w-full h-[230px] ">
+                    <img
+                      src={project.image.url}
+                      alt="project_image"
+                      className="w-full h-full object-cover rounded-2xl "
+                    />
+                  </div>
+                  <div className="mt-5">
+                    <h3 className="text-white font-bold text-[24px]">
+                      {project.title}
+                    </h3>
+                  </div>
                 </div>
-                <div className="mt-5">
-                  <h3 className="text-white font-bold text-[24px]">
-                    {project.title}
-                  </h3>
-                </div>
-              </div>
-            ) : null
-          )}
+              ) : null
+            )
+        ) : (
+          <p>No projects found.</p>
+        )}
       </div>
 
       {showModal && selectedProject && (
